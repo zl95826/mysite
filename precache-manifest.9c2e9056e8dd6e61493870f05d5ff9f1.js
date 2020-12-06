28b033e623c4c266dc739381494a013c69b08747
zl95826 <zl95826@hotmail.com>
2020-12-05 23:06:53 -0800

    Updates

---
+++ precache-manifest.9c2e9056e8dd6e61493870f05d5ff9f1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbfe6891570472f878a2485ea222b8b",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "73170e9ac6bcb450dbba",
+    "url": "/mysite/static/css/3.220ccb42.chunk.css"
+  },
+  {
+    "revision": "d364950ac55d3a2f8325",
+    "url": "/mysite/static/css/8.d501b4cf.chunk.css"
+  },
+  {
+    "revision": "a2acfc91f6f2c48e8270",
+    "url": "/mysite/static/css/main.eefb09c5.chunk.css"
+  },
+  {
+    "revision": "1bd314b6357c752dc23d",
+    "url": "/mysite/static/js/0.b430beba.chunk.js"
+  },
+  {
+    "revision": "73170e9ac6bcb450dbba",
+    "url": "/mysite/static/js/3.b8105146.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mysite/static/js/3.b8105146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa77b51e6fe3da8ed42",
+    "url": "/mysite/static/js/4.7a484ebc.chunk.js"
+  },
+  {
+    "revision": "1f5c0e596fff265fd212",
+    "url": "/mysite/static/js/5.dbcd32c3.chunk.js"
+  },
+  {
+    "revision": "a9ad415d992905c164f1",
+    "url": "/mysite/static/js/6.9e1f4af6.chunk.js"
+  },
+  {
+    "revision": "38921d408811ca689e46",
+    "url": "/mysite/static/js/7.f154194d.chunk.js"
+  },
+  {
+    "revision": "d364950ac55d3a2f8325",
+    "url": "/mysite/static/js/8.e8f24060.chunk.js"
+  },
+  {
+    "revision": "a2acfc91f6f2c48e8270",
+    "url": "/mysite/static/js/main.9dcfe0b0.chunk.js"
+  },
+  {
+    "revision": "6a51be4ea272dbf50cb3",
+    "url": "/mysite/static/js/runtime-main.23c26a75.js"
+  },
+  {
+    "revision": "c44660ebb3ffe79e1fa17ecbd35ddab4",
+    "url": "/mysite/static/media/3T_game.c44660eb.png"
+  },
+  {
+    "revision": "c868296d1bf9dd6ff5c60701d8699de6",
+    "url": "/mysite/static/media/about.c868296d.png"
+  },
+  {
+    "revision": "7709a4f1572f50b5a052ab006be8ff96",
+    "url": "/mysite/static/media/arrow.7709a4f1.png"
+  },
+  {
+    "revision": "50c9d7197711bfcc45cabc17ca4aeaf7",
+    "url": "/mysite/static/media/bb2.50c9d719.png"
+  },
+  {
+    "revision": "2be65e9f1e61499cca07d91782090730",
+    "url": "/mysite/static/media/bg1.2be65e9f.png"
+  },
+  {
+    "revision": "ba70fb3ec8fad4d2ce8de28de8e9364e",
+    "url": "/mysite/static/media/bg2.ba70fb3e.png"
+  },
+  {
+    "revision": "cb795d12fe45a23af1507abee3241bd3",
+    "url": "/mysite/static/media/bg3.cb795d12.png"
+  },
+  {
+    "revision": "bb4403a72ccef4e6aa89b9b56c4a435c",
+    "url": "/mysite/static/media/bg5.bb4403a7.png"
+  },
+  {
+    "revision": "b1e4b957c115250c01da66f358a23ac6",
+    "url": "/mysite/static/media/burger.b1e4b957.png"
+  },
+  {
+    "revision": "6c7805210df13f9934b3fa4b825f6d19",
+    "url": "/mysite/static/media/eCommerce.6c780521.jpg"
+  },
+  {
+    "revision": "8092f02793e379424a52be39a5d71584",
+    "url": "/mysite/static/media/hl.8092f027.png"
+  },
+  {
+    "revision": "e8776b32e27ef4cccc6d08bedade7bea",
+    "url": "/mysite/static/media/mockupATOM1.e8776b32.jpg"
+  },
+  {
+    "revision": "a362815510d7949980dcd806e7904abe",
+    "url": "/mysite/static/media/mod.a3628155.png"
+  },
+  {
+    "revision": "d61ee31d572d171d91d3303bb99da955",
+    "url": "/mysite/static/media/zdg.d61ee31d.png"
+  }
+]);
--- src/components/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from "./Spinner.module.css";
-
-const spinner = () => (
-  <div style={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
-    <div className={classes.loader}>Loading...</div>
-  </div>
-);
-export default spinner;
