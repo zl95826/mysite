bb774e9bbfd0bbd985cbe3a2089890e3c0282350
zl95826 <zl95826@hotmail.com>
2020-12-05 23:59:09 -0800

    issues

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mysite">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
